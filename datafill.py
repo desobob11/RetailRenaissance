3950efcb12d95563b1a297b790ac55bdcc600f66
desobob11 <desmond.obrien@ucalgary.ca>
2023-11-26 14:12:50 -0700

    Just needs contains, shipment, and supplies

--- datafill.py
+++ datafill.py
@@ -2,16 +2,20 @@ import mysql.connector as msql
 from faker import Faker
 import random
 import datetime
-
+import pandas as pd
+import numpy as np
 
 n_warehouse = 10
 n_branch = 15
 n_user = 500
 n_customer = 10000
-
+n_supplier = 10
+n_trans = 100
+n_products = 100
 
 
 def gen_customers(db, curs, n: int) -> None:
+    print("Customer...")
     fk = Faker(["en_CA"])
     query = "INSERT INTO CUSTOMER (customer_id,phone_num,email,first_name,last_name,address) VALUES "
     for i in range(n):
@@ -31,12 +35,13 @@ def gen_customers(db, curs, n: int) -> None:
 
 
 def gen_warehouses(db, curs, n):
+    print("Warehouses...")
     fk = Faker(["en_CA"])
     query = "INSERT INTO WAREHOUSE (warehouse_id,location,capacity) VALUES "
     for i in range(n):
         location = fk.address()
         capacity = random.randint(10000, 100000)
-        tup = "(%d, '%s', '%s')," % (i + 1, location, capacity)
+        tup = "(%d, '%s', %d)," % (i + 1, location, capacity)
         query += tup
     
     query = query[:len(query)-1] + ";"
@@ -45,12 +50,13 @@ def gen_warehouses(db, curs, n):
 
 
 def gen_branch(db, curs, n):
+    print("Branches...")
     fk = Faker(["en_CA"])
     query = "INSERT INTO STORE_BRANCH (branch_id,location,warehouse_id) VALUES "
     for i in range(n):
         location = fk.address()
         warehouse = random.randint(1, n_warehouse)
-        tup = "(%d, '%s', '%s')," % (i + 1, location, warehouse)
+        tup = "(%d, '%s', %s)," % (i + 1, location, warehouse)
         query += tup
     
     query = query[:len(query)-1] + ";"
@@ -62,12 +68,14 @@ def gen_branch(db, curs, n):
 
 
 def gen_user(db, curs, n: int) -> None:
+    print("Users...")
     fk = Faker(["en_CA"])
     dt_start = datetime.datetime(2000, 1, 1)
     dt_end = datetime.datetime.now()
     manager_count = 1
     for i in range(n):
-        manager_check = random.randint(0, 1)
+        manager_check = random.randint(1, 100)
+        branch_id = random.randint(1, n_branch)
         hired_date = fk.date_between_dates(date_start=dt_start, date_end=dt_end)
 
         email = fk.email().split("@")[0] + "@RR.ca"
@@ -82,8 +90,11 @@ def gen_user(db, curs, n: int) -> None:
         insert_employee = "INSERT INTO EMPLOYEE (user_id, date_hired) VALUES (%s, '%s')" % (i + 1, hired_date)
         curs.execute(insert_employee)
 
-        if manager_check:
-            insert_manager = "INSERT INTO MANAGER (user_id, manager_id, branch_id) VALUES (%s, %s, %s)" % (i + 1, manager_count, random.randint(1, n_branch))
+        insert_works_at = "INSERT INTO WORKS_AT (user_id, branch_id) VALUES (%s, %s)" % (i + 1, branch_id)
+        curs.execute(insert_works_at)
+
+        if manager_check > 75:
+            insert_manager = "INSERT INTO MANAGER (user_id, manager_id, branch_id) VALUES (%s, %s, %s)" % (i + 1, manager_count, branch_id)
             curs.execute(insert_manager)
             manager_count += 1
 
@@ -91,9 +102,110 @@ def gen_user(db, curs, n: int) -> None:
         db.commit()
 
 
+def gen_product(db, curs, n):
+    print("Products...")
+    fk = Faker(["en_CA"])
+    product_names = []
+    with open("products.txt", "r") as file:
+        product_names = file.readlines()
+
+    query = "INSERT INTO PRODUCT (product_id,product_name,num_stock, price, branch_id) VALUES "
+    for i in range(len(product_names)):
+        name = product_names[i]
+        num_stock = random.randint(100, 1000)
+        price = float(random.randint(1, 1000)) - 0.01
+        warehouse = random.randint(1, n_warehouse)
+        tup = "(default, '%s', %s, %s, %s)," % (name, num_stock, price, warehouse)
+        query += tup
+    
+
+    query = query[:len(query)-1] + ";"
+    curs.execute(query)
+    db.commit()
+
+
+def gen_supplier(db, curs, n):
+    print("Suppliers...")
+    fk = Faker(["en_CA"])
+    query = "INSERT INTO SUPPLIER (supplier_id,company_name,address,phone,email) VALUES "
+    for i in range(n):
+        location = fk.address()
+        name = fk.company()
+        phone = fk.phone_number()
+        email = fk.email()
+        warehouse = random.randint(1, n_warehouse)
+        tup = "(default, '%s', '%s', '%s', '%s')," % (name, location, phone, email)
+        query += tup
+    
+    query = query[:len(query)-1] + ";"
+    curs.execute(query)
+    db.commit()
+
+
+
+def gen_transactions(db, curs, n):
+    print("Transactions...")
+    fk = Faker(["en_CA"])
+    dt_start = datetime.datetime(2000, 1, 1)
+    dt_end = datetime.datetime.now()
+    statuses = ["Complete", "Pending", "Cancelled"]
+    prod_table = query(curs, "SELECT * FROM PRODUCT;")
+    for i in range(n):
+        print(i)
+
+
+
+        customer = random.randint(1, n_customer)
+        user = random.randint(1, n_user)
+        trans_date = fk.date_between_dates(date_start=dt_start, date_end=dt_end)
+        status = statuses[random.randint(0, 2)]
+        insert_trans = "INSERT INTO `TRANSACTION` (transaction_id, amount, transaction_date, customer_id, status) VALUES (%d, 0, '%s', %s, '%s');"% (i + 1, trans_date, customer, status)
+        
+        curs.execute(insert_trans)
+
+        insert_order = "INSERT INTO `ORDER` (transaction_id, user_id, order_status) VALUES (%d, %d, '%s');" %  (i + 1, user, status)
+        curs.execute(insert_order)
+
+
+
+        
+        num_unique_prods = random.randint(1, 10)
+        prods = []
+        n_prods = []
+
+        # get products to buy
+        while len(prods) < num_unique_prods:
+            prod_id = random.randint(1, n_products)
+            if prod_id not in prods:
+                prods.append(prod_id)
+
+        while len(n_prods) < num_unique_prods:
+            n_prods.append(random.randint(1, 10))
+
+        amount = 0
+        for j in range(num_unique_prods):
+            price = prod_table[prod_table["product_id"] == prods[j]]["price"].values[0]
+            amount += price * n_prods[j]
+            update = "UPDATE `TRANSACTION` SET amount=%s WHERE transaction_id=%d;" % (i + 1, price * n_prods[j])
+            curs.execute(update)
+            insert_requires = "INSERT INTO `REQUIRES` (transaction_id, product_id, num_prods) VALUES (%d, %d, %d);" %  (i + 1, prods[j], n_prods[j])
+            curs.execute(insert_requires)
+
+        update = "UPDATE `TRANSACTION` SET amount=%s WHERE transaction_id=%d;" % (amount, i + 1)
+        curs.execute(update)
+
+
+    db.commit()
+
+
 
 
     
+
+def query(curs, query):
+    curs.execute(query)
+    return pd.DataFrame(curs.fetchall(), columns=[i[0] for i in curs.description])
+
 
 
 
@@ -111,11 +223,16 @@ def main():
     )
     curs = db.cursor()
 
-   # gen_customers(db, curs, n_customer)
-    #gen_user(db, curs, n_user);
+    
+    #gen_customers(db, curs, n_customer)
     #gen_warehouses(db, curs, n_warehouse)
     #gen_branch(db, curs, n_branch)
-    gen_user(db, curs, 50);
+    #gen_user(db, curs, n_user)
+    #gen_product(db, curs, 0)
+    #gen_supplier(db, curs, n_supplier)
+    gen_transactions(db, curs, 1)
+
+
 
 
 
--- test.sql
+++ test.sql
@@ -1,11 +1,100 @@
-INSERT INTO CUSTOMER (customer_id,phone_num,email,first_name,last_name,address) VALUES (default, '(183) 751-3373', 'shelby50@example.org', 'Ruth', 'Walker', '5120 Miller Centers
-Richland, NT E7C5M5'),(default, '(605) 615-8758', 'jacqueline98@example.org', 'William', 'Coleman', '9059 Williams Shore Suite 018
-Rodriguezmouth, SK B6B2A7'),(default, '(280) 909-7107', 'aliciajackson@example.net', 'James', 'White', '9655 Sara Junction
-Jenkinsburgh, NB E6P 8L9'),(default, '173 716 5527', 'griffinrichard@example.net', 'Kelly', 'Hanson', '12086 Pham Camp Apt. 523
-South Amy, AB J7B5L3'),(default, '397 530 8879', 'gillkevin@example.org', 'Benjamin', 'Williams', '401 Bridges Trafficway
-West Mark, PE P8N 2S9'),(default, '+1 (415) 468-7383', 'timothy28@example.com', 'Fernando', 'Smith', '95551 Nancy Ridge
-Lake Aprilport, MB T1J9V7'),(default, '1 (659) 910-7014', 'owenmark@example.org', 'Oscar', 'Williams', '063 Brittany Well
-Lisaview, SK X8P 4J8'),(default, '(624) 943-9704 x093', 'tylerlee@example.com', 'Jeff', 'Duncan', '302 Hale Lodge Suite 166
-New Kimchester, PE C4H9T8'),(default, '145.614.3351', 'stephanie33@example.com', 'Darius', 'Potter', '917 Edwards Brook
-Catherineland, NL T6T 5G1'),(default, '925 624 4390', 'lesliehoward@example.org', 'Kathleen', 'Coleman', '32825 Jones Square Apt. 438
-Goodwinchester, QC S2H6J8');
+INSERT INTO PRODUCT (product_id,product_name,num_stock, price, branch_id) VALUES (default, 'EcoGlow Thermostat
+', 739, 867.99, 3),(default, 'HarmonyLink Doorbell
+', 835, 442.99, 7),(default, 'AquaSavor Faucet
+', 120, 434.99, 8),(default, 'LumiSense Light Bulbs
+', 754, 534.99, 2),(default, 'ZenGuard Security System
+', 747, 123.99, 5),(default, 'PulseFlow Ceiling Fan
+', 110, 1.99, 6),(default, 'EcoVent Air Vent
+', 590, 258.99, 6),(default, 'SafeLock Door Lock
+', 262, 994.99, 7),(default, 'VistaView Window Blinds
+', 928, 483.99, 5),(default, 'SerenityHub Home Hub
+', 521, 856.99, 1),(default, 'Sculptura Door Handles
+', 876, 592.99, 4),(default, 'CasaSense Smoke Detector
+', 553, 787.99, 10),(default, 'ChromaGlo LED Strips
+', 384, 707.99, 7),(default, 'SonicSurge Door Chime
+', 466, 510.99, 6),(default, 'AeroBreeze Air Purifier
+', 617, 666.99, 6),(default, 'CasaConnect Power Strip
+', 935, 785.99, 5),(default, 'HarborHeat Radiator
+', 286, 905.99, 6),(default, 'EcoSprout Sprinkler System
+', 604, 597.99, 5),(default, 'VitaSync Plant Monitor
+', 411, 52.99, 6),(default, 'PurityFlow Water Purifier
+', 399, 35.99, 6),(default, 'AeroCirc Ceiling Fan
+', 871, 886.99, 5),(default, 'SolaraShade Window Shades
+', 192, 870.99, 6),(default, 'TranquilTemp Thermostatic Valve
+', 479, 382.99, 2),(default, 'LuxeLink Smart Mirror
+', 432, 870.99, 5),(default, 'AquaSense Shower Head
+', 365, 121.99, 10),(default, 'SpectraSync Picture Frame
+', 132, 778.99, 7),(default, 'CasaView Security Camera
+', 140, 249.99, 8),(default, 'NovaNest Home Starter Kit
+', 106, 192.99, 6),(default, 'AeroGlide Curtain Rail
+', 802, 2.99, 7),(default, 'ZenNook Bookshelf
+', 280, 96.99, 3),(default, 'CasaPet Pet Feeder
+', 734, 994.99, 10),(default, 'LumiLink Table Lamp
+', 315, 37.99, 9),(default, 'SensiSync HVAC System
+', 430, 743.99, 8),(default, 'EchoGrove Garden Monitor
+', 485, 311.99, 8),(default, 'HarborHub Ethernet Switch
+', 955, 288.99, 8),(default, 'VivaVox Voice Assistant
+', 490, 32.99, 9),(default, 'CasaChef Kitchen Appliance Set
+', 948, 924.99, 8),(default, 'VeloVolt Electrical Outlets
+', 877, 185.99, 2),(default, 'EcoHaven Waste Management
+', 792, 531.99, 7),(default, 'AeroPod Plant Pod
+', 621, 763.99, 5),(default, 'LumiLift Adjustable Lighting
+', 531, 621.99, 1),(default, 'PulsePurr Pet Collar
+', 112, 865.99, 3),(default, 'CasaCare First Aid Kit
+', 694, 334.99, 5),(default, 'HarborHush Noise Cancelling Device
+', 195, 219.99, 4),(default, 'SculpturaScape Art Display
+', 723, 614.99, 10),(default, 'AquaHarmony Water Leak Detector
+', 424, 935.99, 4),(default, 'EcoHarbor Energy Monitor
+', 534, 878.99, 3),(default, 'LuxeLife Luxury Home System
+', 384, 958.99, 2),(default, 'PuroPulse Pulse Oximeter
+', 314, 999.99, 9),(default, 'AeroGlow Night Light
+', 351, 719.99, 3),(default, 'AuraSync Smart Mirror
+', 679, 853.99, 8),(default, 'SparkLink Smart Sparkler
+', 476, 68.99, 8),(default, 'QuantumQuench Smart Cooler
+', 753, 775.99, 7),(default, 'SolarSync Solar Charger
+', 725, 199.99, 1),(default, 'AirNest Smart Mattress
+', 920, 567.99, 4),(default, 'TerraGuard Smart Security Fence
+', 348, 746.99, 3),(default, 'AquaBlast Smart Pressure Washer
+', 435, 447.99, 4),(default, 'PulsePod Smart Speaker System
+', 307, 484.99, 9),(default, 'SyncWave Smart Curtain System
+', 197, 114.99, 2),(default, 'ZenVibe Smart Meditation Cushion
+', 110, 379.99, 5),(default, 'SwiftSense Smart Motion Sensor
+', 584, 678.99, 1),(default, 'SolarScape Solar-Powered Lights
+', 397, 525.99, 9),(default, 'PulsePro Smart Fitness Tracker
+', 784, 946.99, 2),(default, 'AquaFlare Smart Water Dispenser
+', 649, 176.99, 6),(default, 'ZenNap Smart Sleep Mask
+', 153, 286.99, 4),(default, 'EchoHive Smart Hive System
+', 728, 102.99, 8),(default, 'EcoCharge Smart Battery Charger
+', 746, 389.99, 8),(default, 'CelestialShade Smart Skylight
+', 245, 183.99, 8),(default, 'QuantumShift Smart Gearbox
+', 353, 781.99, 6),(default, 'SolarSail Smart Sunshade
+', 507, 558.99, 3),(default, 'AquaTide Smart Tide Predictor
+', 640, 595.99, 5),(default, 'ZenFlick Smart Light Switch
+', 558, 7.99, 2),(default, 'PulsePetal Smart Flower Pot
+', 984, 963.99, 3),(default, 'AeroLume Smart Candle Holder
+', 502, 935.99, 8),(default, 'VeloView Smart Bike Rack
+', 810, 183.99, 4),(default, 'EchoPuff Smart Air Freshener
+', 706, 154.99, 6),(default, 'SolarSync Smart Garden Lights
+', 987, 332.99, 10),(default, 'AquaGlide Smart Pool Cleaner
+', 121, 915.99, 1),(default, 'PulseGaze Smart Eye Tracker
+', 646, 709.99, 6),(default, 'ZenBlend Smart Blender
+', 523, 256.99, 5),(default, 'EcoSwift Smart Delivery Box
+', 805, 199.99, 1),(default, 'CelestialWave Smart Ocean Sounds
+', 106, 580.99, 9),(default, 'QuantumRide Smart Bike
+', 447, 286.99, 7),(default, 'TerraTorch Smart Outdoor Heater
+', 380, 707.99, 1),(default, 'SolarLoom Smart Window Curtain
+', 417, 481.99, 8),(default, 'VeloRack Smart Bike Storage
+', 540, 77.99, 10),(default, 'PulsePress Smart Coffee Maker
+', 918, 742.99, 1),(default, 'ZenTwirl Smart Ceiling Fan
+', 461, 888.99, 9),(default, 'AquaChill Smart Beverage Cooler
+', 384, 223.99, 7),(default, 'CelestialPulse Smart Sky Projector
+', 457, 678.99, 8),(default, 'QuantumGlow Smart Glow Paint
+', 892, 145.99, 4),(default, 'SolarSync Smart Plant Waterer
+', 627, 724.99, 1),(default, 'VeloPulse Smart Bike Lock
+', 531, 275.99, 8),(default, 'EchoNudge Smart Reminder Device
+', 695, 763.99, 6),(default, 'AquaGlow Smart Fish Tank
+', 347, 123.99, 8),(default, 'PulseScribe Smart Notebook
+', 204, 889.99, 8),(default, 'ZenFloat Smart Floating Shelf
+', 520, 945.99, 1),(default, 'TerraPulse Smart Soil Moisture Sensor
+', 206, 126.99, 9),(default, 'SolarLuxe Smart Window Film
+', 306, 810.99, 3),(default, 'AeroSprout Smart', 957, 286.99, 7);
